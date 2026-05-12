3bf4e11803ae5b819a66770c26cb1e10438f1dc6
pbio <10051819+paulbalaji@users.noreply.github.com>
2026-05-12 15:48:16 +0100

    feat(igp): support legacy IGP config

--- typescript/infra/config/environments/mainnet3/core.ts
+++ typescript/infra/config/environments/mainnet3/core.ts
@@ -23,6 +23,7 @@ import {
 import { Address, objMap } from '@hyperlane-xyz/utils';
 
 import { getChain } from '../../registry.js';
+import { legacyIgpChains } from '../../../src/config/chain.js';
 
 import { getEdenCoreConfig } from './eden.js';
 import { getTronCoreConfig } from './tron.js';
@@ -171,6 +172,7 @@ export const core: ChainMap<CoreConfig> = objMap(
       defaultIsm,
       defaultHook,
       requiredHook,
+      ...(legacyIgpChains.includes(local) ? { deployQuotedCalls: false } : {}),
       ...owner,
     };
   },
